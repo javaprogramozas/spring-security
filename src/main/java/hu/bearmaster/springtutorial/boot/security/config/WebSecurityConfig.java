fe3847f0d0a94a2dba32ec9cd6f2150b957508b6
javaprogramozas <javaprogramozas.hu@gmail.com>
2024-02-13 22:27:25 +0100

    [v74] Külső felhasználói azonosítás (OAuth)

--- src/main/java/hu/bearmaster/springtutorial/boot/security/config/WebSecurityConfig.java
+++ src/main/java/hu/bearmaster/springtutorial/boot/security/config/WebSecurityConfig.java
@@ -5,11 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @EnableMethodSecurity
 @Configuration
@@ -27,13 +34,26 @@ public class WebSecurityConfig {
 			.formLogin(login -> login
 					.loginPage("/login")
 					.permitAll())
-			.oauth2Login(oauth -> oauth.loginPage("/login"))
+			.oauth2Login(oauth -> oauth.loginPage("/login")
+					.userInfoEndpoint(userInfo -> userInfo
+							.userAuthoritiesMapper(myAuthMapper())))
 			.logout(logout -> logout
 					.logoutUrl("/logout")
 					.logoutSuccessUrl("/login?logout")
 					.permitAll());
 
 		return http.build();
+	}
+
+	private GrantedAuthoritiesMapper myAuthMapper() {
+		return authorities -> {
+			for (GrantedAuthority authority : authorities) {
+				if (authority instanceof OAuth2UserAuthority) {
+					return Set.of(new SimpleGrantedAuthority("ROLE_USER"));
+				}
+			}
+			return authorities;
+		};
 	}
 
 	@Bean
